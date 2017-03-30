1f7ac1da6243d97236cc168f7f1aceddcba4b048
Bastien Burri <bastien.burri@he-arc.ch>
2017-03-30 17:43:30 +0200

    Mise à jour de la vue lorsqu'on clique sur le bouton de filtre de vidéos (ne fonctionne pas encore)

---
+++ app/assets/javascripts/galerie.js
@@ -0,0 +1,11 @@
+jQuery(document).ready(function() {
+	$('#btn-videos').on('click', function(e){
+    e.preventDefault();
+    $.ajax({
+    type:'GET',
+    url:'/galerie',
+    dataType: 'json',
+    data: { 'type' : "videos", }
+    });
+	});
+});
--- app/controllers/galeries_controller.rb
+++ app/controllers/galeries_controller.rb
@@ -1,10 +1,21 @@
 class GaleriesController < ApplicationController
   def index
-    @galeries = Galerie.order('created_at DESC')
-    @pictures = Picture.order('created_at DESC')
-    @medias = (@galeries + @pictures).sort_by { |model| model.created_at }.in_groups_of(3)
+  	p params[:type]
+  	 case params[:type]
+	 when "videos"  
+	 	@medias = Galerie.order('created_at DESC')
+ 	 when "images" 
+ 	    @medias = Picture.order('created_at DESC')
+ 	 else 
+ 	 	@medias = Galerie.order('created_at DESC').in_groups_of(3)
+ 	 end
 
+  #  @galeries = Galerie.order('created_at DESC')
+  #  @pictures = Picture.order('created_at DESC')
+  #  @medias = (@galeries + @pictures).sort_by { |model| model.created_at }.in_groups_of(3)
   end
+
+
 
   def new
     @galerie = Galerie.new
