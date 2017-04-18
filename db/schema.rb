570795d6ed279c4e45e34f8d0f8a23d8d280f740
matthieu.bandelie <matbandeli@gmail.com>
2017-04-18 22:09:47 +0200

    shema update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170417122026) do
+ActiveRecord::Schema.define(version: 20170418113407) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,11 @@ ActiveRecord::Schema.define(version: 20170417122026) do
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  create_table "carrousels", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "categories", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
@@ -74,7 +79,9 @@ ActiveRecord::Schema.define(version: 20170417122026) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.integer  "category_id"
     t.integer  "photosvideo_id"
+    t.index ["category_id"], name: "index_pictures_on_category_id", using: :btree
     t.index ["photosvideo_id"], name: "index_pictures_on_photosvideo_id", using: :btree
   end
 
@@ -102,5 +109,6 @@ ActiveRecord::Schema.define(version: 20170417122026) do
 
   add_foreign_key "galeries", "photosvideos"
   add_foreign_key "photosvideos", "types"
+  add_foreign_key "pictures", "categories"
   add_foreign_key "pictures", "photosvideos"
 end
