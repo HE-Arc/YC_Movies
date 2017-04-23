3ce8a146ebc18145c455407e341bd79cf7045b04
damien.gygi <gygi.damien@gmail.com>
2017-04-23 19:21:18 +0200

    amélioration seed carrousel

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,35 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
-Type.create!(name: 'Picture')
-Type.create!(name: 'Video')
-Captcha.create!(activate: false)
+#AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+#Type.create!(name: 'Picture')
+#Type.create!(name: 'Video')
+#Captcha.create!(activate: false)
 
 #Category.delete_all
 #Category.create("Reportages AfterMovies Commerciales Mariages Creations".split.map { |name| {name: name} })
 
-Carrousel.create({
-  :id => 1,
-  :image1_file_name => "carrousel_1_750_350_000000.png",
-  :image2_file_name => "carrousel_1_1220_500_000000.png",
-  :image3_file_name => "carrousel_1_1520_550_000000.png",
-  :image4_file_name => "carrousel_1_1920_624_000000.png"
-})
+Carrousel.create(image1: File.new("#{Rails.root}/app/assets/images/Carrousel/carrousel_1_750_350_000000.png"),
+    image2: File.new("#{Rails.root}/app/assets/images/Carrousel/carrousel_1_1220_500_000000.png"),
+    image3: File.new("#{Rails.root}/app/assets/images/Carrousel/carrousel_1_1520_550_000000.png"),
+    image4: File.new("#{Rails.root}/app/assets/images/Carrousel/carrousel_1_1920_624_000000.png"))
 
-Carrousel.create({
-  :id => 2,
-  :image1_file_name => "carrousel_2_750_350_000000.png",
-  :image2_file_name => "carrousel_2_1220_500_000000.png",
-  :image3_file_name => "carrousel_2_1520_550_000000.png",
-  :image4_file_name => "carrousel_2_1920_624_000000.png"
-})
+Carrousel.create(image1: File.new("#{Rails.root}/app/assets/images/Carrousel/carrousel_2_750_350_000000.png"),
+    image2: File.new("#{Rails.root}/app/assets/images/Carrousel/carrousel_2_1220_500_000000.png"),
+    image3: File.new("#{Rails.root}/app/assets/images/Carrousel/carrousel_2_1520_550_000000.png"),
+    image4: File.new("#{Rails.root}/app/assets/images/Carrousel/carrousel_2_1920_624_000000.png"))
 
-Category.create({
-    :id => 5,
-    :name => "Creations",
-    :image1_file_name => "Theme_5_creations_736_200_000000.png",
-    :image2_file_name => "Theme_5_creations_640_200_000000.png",
-    :image3_file_name => "Theme_5_creations_288_200_000000.png",
-    :image4_file_name => "Theme_5_creations_992_200_000000.png"
-})
+Carrousel.create(image1: File.new("#{Rails.root}/app/assets/images/Carrousel/carrousel_3_750_350_000000.png"),
+    image2: File.new("#{Rails.root}/app/assets/images/Carrousel/carrousel_3_1220_500_000000.png"),
+    image3: File.new("#{Rails.root}/app/assets/images/Carrousel/carrousel_3_1520_550_000000.png"),
+    image4: File.new("#{Rails.root}/app/assets/images/Carrousel/carrousel_3_1920_624_000000.png"))
