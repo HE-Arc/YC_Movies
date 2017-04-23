520174228f2c5708cd1d035ca4a872bfb095a61e
damien.gygi <gygi.damien@gmail.com>
2017-04-23 14:06:06 +0200

    seed carrousel

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,3 +12,19 @@ Captcha.create!(activate: false)
 
 #Category.delete_all
 #Category.create("Reportages AfterMovies Commerciales Mariages Creations".split.map { |name| {name: name} })
+
+Carrousel.create({
+  :id => 1,
+  :image1_file_name => "carrousel_1_750_350_000000.png",
+  :image2_file_name => "carrousel_1_1220_500_000000.png",
+  :image3_file_name => "carrousel_1_1520_550_000000.png",
+  :image4_file_name => "carrousel_1_1920_624_000000.png"
+})
+
+Carrousel.create({
+  :id => 2,
+  :image1_file_name => "carrousel_2_750_350_000000.png",
+  :image2_file_name => "carrousel_2_1220_500_000000.png",
+  :image3_file_name => "carrousel_2_1520_550_000000.png",
+  :image4_file_name => "carrousel_2_1920_624_000000.png"
+})
