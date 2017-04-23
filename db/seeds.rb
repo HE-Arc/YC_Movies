470298591ad92a93a4f3adaabfe7c331ca595c0c
damien.gygi <gygi.damien@gmail.com>
2017-04-23 19:55:30 +0200

    seed categorie

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-#AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
-#Type.create!(name: 'Picture')
-#Type.create!(name: 'Video')
-#Captcha.create!(activate: false)
+AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+Type.create!(name: 'Picture')
+Type.create!(name: 'Video')
+Captcha.create!(activate: false)
 
 #Category.delete_all
 #Category.create("Reportages AfterMovies Commerciales Mariages Creations".split.map { |name| {name: name} })
@@ -27,3 +27,33 @@ Carrousel.create(image1: File.new("#{Rails.root}/app/assets/images/Carrousel/car
     image2: File.new("#{Rails.root}/app/assets/images/Carrousel/carrousel_3_1220_500_000000.png"),
     image3: File.new("#{Rails.root}/app/assets/images/Carrousel/carrousel_3_1520_550_000000.png"),
     image4: File.new("#{Rails.root}/app/assets/images/Carrousel/carrousel_3_1920_624_000000.png"))
+
+Category.create( name: "Reportages",
+    image1: File.new("#{Rails.root}/app/assets/images/Themes/Theme_1_reportages_736_200_000000.png"),
+    image2: File.new("#{Rails.root}/app/assets/images/Themes/Theme_1_reportages_640_200_000000.png"),
+    image3: File.new("#{Rails.root}/app/assets/images/Themes/Theme_1_reportages_288_200_000000.png"),
+    image4: File.new("#{Rails.root}/app/assets/images/Themes/Theme_1_reportages_922_200_000000.png"))
+
+Category.create( name: "Aftermovies",
+    image1: File.new("#{Rails.root}/app/assets/images/Themes/Theme_2_aftermovies_736_200_000000.png"),
+    image2: File.new("#{Rails.root}/app/assets/images/Themes/Theme_2_aftermovies_640_200_000000.png"),
+    image3: File.new("#{Rails.root}/app/assets/images/Themes/Theme_2_aftermovies_288_200_000000.png"),
+    image4: File.new("#{Rails.root}/app/assets/images/Themes/Theme_2_aftermovies_922_200_000000.png"))
+
+Category.create( name: "Commerciales",
+    image1: File.new("#{Rails.root}/app/assets/images/Themes/Theme_3_commerciales_736_200_000000.png"),
+    image2: File.new("#{Rails.root}/app/assets/images/Themes/Theme_3_commerciales_640_200_000000.png"),
+    image3: File.new("#{Rails.root}/app/assets/images/Themes/Theme_3_commerciales_288_200_000000.png"),
+    image4: File.new("#{Rails.root}/app/assets/images/Themes/Theme_3_commerciales_922_200_000000.png"))
+
+Category.create( name: "Mariages",
+    image1: File.new("#{Rails.root}/app/assets/images/Themes/Theme_4_mariages_736_200_000000.png"),
+    image2: File.new("#{Rails.root}/app/assets/images/Themes/Theme_4_mariages_640_200_000000.png"),
+    image3: File.new("#{Rails.root}/app/assets/images/Themes/Theme_4_mariages_288_200_000000.png"),
+    image4: File.new("#{Rails.root}/app/assets/images/Themes/Theme_4_mariages_922_200_000000.png"))
+
+Category.create( name: "Creations",
+    image1: File.new("#{Rails.root}/app/assets/images/Themes/Theme_5_creations_736_200_000000.png"),
+    image2: File.new("#{Rails.root}/app/assets/images/Themes/Theme_5_creations_640_200_000000.png"),
+    image3: File.new("#{Rails.root}/app/assets/images/Themes/Theme_5_creations_288_200_000000.png"),
+    image4: File.new("#{Rails.root}/app/assets/images/Themes/Theme_5_creations_992_200_000000.png"))
